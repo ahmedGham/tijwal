3f393e94d6eca9f255fe8b755de371bf3a5e00fe
AhmedGham <ahmed.ghamdhan2018@gmail.com>
2025-09-25 22:31:53 +0300

    ignoreBuildErrors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  typescript: {
+    // ⚠️ This will let builds succeed even with type errors
+    ignoreBuildErrors: true,
+  },
 };
 
 module.exports = nextConfig;
